c0dcf29dc4c8835845f33781443118800b0bf45a
mhlebens <marifermatah@gmail.com>
2025-02-25 08:06:19 -0600

    Agregué el archivo de la práctica 2 al repositorio

---
+++ SistemaMedico/src/ClasePrincipal.java
@@ -0,0 +1,16 @@
+
+
+/**
+ *
+ * @author marif
+ */
+public class ClasePrincipal {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
