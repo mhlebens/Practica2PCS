d4ac22d89f3ed2a0710e4cc47b3db88995682c31
Pablo Arias <parias40359@ufide.ac.cr>
2025-03-02 01:24:52 -0600

    me cago en el Push en NetBeans

--- SistemaMedico/src/Archivo.java
+++ SistemaMedico/src/Archivo.java
@@ -1,12 +1,17 @@
+import java.io.FileOutputStream;
+import java.io.OutputStreamWriter;
+import java.io.IOException;
 
-
-/**
- *
- * @author marif
- */
 public class Archivo {
-    
-    
-    
-    
+    private static final String ARCHIVO = "pacientes.txt";
+
+    public static void agregarPaciente(Paciente paciente) {
+        try (FileOutputStream fos = new FileOutputStream(ARCHIVO, true);
+             OutputStreamWriter writer = new OutputStreamWriter(fos)) {
+            writer.write(paciente.toString()); 
+            writer.write("\n");
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
 }
