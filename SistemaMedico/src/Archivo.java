d0df61f6c4c0804722b24b2b18d94bc4c33c8242
evildead191 <parias40359@ufide.ac.cr>
2025-03-02 20:12:07 -0600

    Se cambió Archivo para hacer uso de dat

--- SistemaMedico/src/Archivo.java
+++ SistemaMedico/src/Archivo.java
@@ -1,15 +1,19 @@
+import java.io.DataOutputStream;
 import java.io.FileOutputStream;
-import java.io.OutputStreamWriter;
 import java.io.IOException;
 
 public class Archivo {
-    private static final String ARCHIVO = "pacientes.txt";
+    private static final String ARCHIVO = "pacientes.dat";
 
     public static void agregarPaciente(Paciente paciente) {
         try (FileOutputStream fos = new FileOutputStream(ARCHIVO, true);
-             OutputStreamWriter writer = new OutputStreamWriter(fos)) {
-            writer.write(paciente.toString()); 
-            writer.write("\n");
+             DataOutputStream dos = new DataOutputStream(fos)) {
+            dos.writeUTF(paciente.getCedula());
+            dos.writeUTF(paciente.getNombre());
+            dos.writeUTF(paciente.getSintomas());
+            dos.writeUTF(paciente.getDiagnostico());
+            dos.writeUTF(paciente.getTratamiento());
+            
             System.out.println("Paciente agregado: " + paciente.toString());
 
         } catch (IOException e) {
